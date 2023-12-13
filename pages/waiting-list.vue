46bdc7e167e591fbb1f854e077c270815590697c
Naiky <74ding@gmail.com>
2023-12-13 14:44:05 +0800

    feat: waiting list page

--- pages/waiting-list.vue
+++ pages/waiting-list.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="text-3xl font-bold underline bg-amber-900">Hello world!</h1>
   </div>
 </template>
 
