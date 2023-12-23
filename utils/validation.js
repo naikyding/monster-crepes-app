884cb69b49b7df20bb4647e5c63791743f4206b7
Naiky <74ding@gmail.com>
2023-12-23 14:00:48 +0800

    feat: 未三碼驗証

--- utils/validation.js
+++ utils/validation.js
@@ -1,16 +1,13 @@
 import { useField } from "vee-validate";
+import { isNumeric, isLength } from "validator";
 
 // 手機未三碼
 export const checkLastThreeDigits = (inputName) =>
-  useField(inputName || "last3Digits", (value) => {
-    if (!value) {
-      return "手機未三碼必填";
-    }
-    if (value.length < 3) {
-      return "手機未三碼不足 3 位數";
-    }
-    if (value.length > 3) {
-      return "手機未三碼超過 3 位數";
-    }
+  useField(inputName || "last3Digits", (mobileLast3Number) => {
+    if (!mobileLast3Number) return "手機未三碼必填";
+    if (!isNumeric(mobileLast3Number)) return "請輸入數字格式";
+    if (!isLength(mobileLast3Number, { min: 3 }))
+      return "手機未三碼長度錯誤 (最少三碼)";
+
     return true;
   });
